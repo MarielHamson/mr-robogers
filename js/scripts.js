93743fc4fc1d734af3c075e0f10af75b56e3e2a1
Mariel Hamson <mariel.hamson@gmail.com>
2020-06-12 10:55:08 -0700

    complete logic for branching and substitution code, loop through array looking for certain strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,34 @@
-$(document).ready(function() {
+// business logic
+
+const robotese = function(translation){
+
+  let robotArray = [];
+
+  for (let index = 0; index <= translation; index += 1) {
+
+      const str = index.toString();
+
+      if (str.includes("3") === true) {
+          robotArray.push(" Won't you be my neighbor?");
+      } else if (str.includes("2") === true) {
+          robotArray.push(" Boop!");
+      } else if (str.includes("1") === true) {
+          robotArray.push(" Beep!");
+      } else {
+          robotArray.push(" " + str);
+      }
+    }
+
+  return robotArray;
+  }
+
 // UI logic
+
+$(document).ready(function() {
   $(".form-inline").submit(function(event) {
+      event.preventDefault();
       firstName = $("input#name").val();
       $("#robot-lang").show();
-      event.preventDefault();
   });
 
   $("form#robot-lang").submit(function() {
