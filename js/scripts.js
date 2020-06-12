8c4018c5b9651247272a98d619748ff9d588c7fd
Mariel Hamson <mariel.hamson@gmail.com>
2020-06-12 14:38:07 -0700

    refactor / beautify JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,58 @@
 // business logic
 
 const robotese = function(numberInput){
-
   let robotArray = [];
-
   for (let index = 0; index <= numberInput; index += 1) {
-
-      const str = index.toString();
-
-      if (str.includes("3") === true) {
-          robotArray.push(" Won't you be my neighbor? ");
-      } else if (str.includes("2") === true) {
-          robotArray.push(" Boop!");
-      } else if (str.includes("1") === true) {
-          robotArray.push(" Beep!");
-      } else {
-          robotArray.push(" " + str);
-      }
+    const str = index.toString();
+    if (str.includes("3") === true) {
+      robotArray.push(" Won't you be my neighbor? ");
+  } else if (str.includes("2") === true) {
+      robotArray.push(" Boop!");
+  } else if (str.includes("1") === true) {
+      robotArray.push(" Beep!");
+    } else {
+      robotArray.push(" " + str);
     }
+  }
 
   return robotArray;
-  }
+  };
 
 const backwardsRobotese = function(reverseNumberInput){
     let reversedRobotArray = [];
-  
     for (let index = reverseNumberInput; index >= 0; index -= 1) {
-  
-        const str = index.toString();
-  
-        if (str.includes("3") === true) {
-            reversedRobotArray.push(" Won't you be my neighbor? ");
-        } else if (str.includes("2") === true) {
-            reversedRobotArray.push(" Boop!");
-        } else if (str.includes("1") === true) {
-            reversedRobotArray.push(" Beep!");
-        } else {
-            reversedRobotArray.push(" " + str);
+      const str = index.toString();
+      if (str.includes("3") === true) {
+        reversedRobotArray.push(" Won't you be my neighbor? ");
+    } else if (str.includes("2") === true) {
+        reversedRobotArray.push(" Boop!");
+    } else if (str.includes("1") === true) {
+        reversedRobotArray.push(" Beep!");
+    } else {
+        reversedRobotArray.push(" " + str);
         }
       }
-  
+
     return reversedRobotArray;
-    }
+    };
 
 // UI logic
 
 $(document).ready(function() {
   $(".form-inline").submit(function(event) {
-      event.preventDefault();
-      firstName = $("input#name").val();
-      $("#robot-lang").show();
+    event.preventDefault();
+    firstName = $("input#name").val();
+    $("#robot-lang").show();
   });
 
   $("form#robot-lang").submit(function(event) {
-      event.preventDefault();
-      const numberInput = $("#numberInput").val();
-      const result = robotese(numberInput);
-      $(".translation").text("Dearest " + firstName + ": " + result);
-      $("#result").slideDown("slow");
-      $("#reversal").show();
-      $("#reversed-result").hide();
+    event.preventDefault();
+    const numberInput = $("#numberInput").val();
+    const result = robotese(numberInput);
+    $(".translation").text("Dearest " + firstName + ": " + result);
+    $("#result").slideDown("slow");
+    $("#reversal").show();
+    $("#reversed-result").hide();
   });
 
   $("button#reverse-button").click(function(event) {
